ba22034a4446ee6c319adefde3e6d38baf182033
yeah1Choi <choiwon0122@gmail.com>
2023-11-13 21:58:07 +0900

    231113

---
+++ HELLO_AJAX/HELLO_AJAX/daoemp.py
@@ -0,0 +1,68 @@
+import pymysql
+class DaoEmp:
+    def __init__(self):
+        self.conn = pymysql.connect(
+            host='127.0.0.1',
+            user='root', 
+            password='python', 
+            db='python', 
+            charset='utf8',
+            port = 3305
+        )
+        self.cur = self.conn.cursor(pymysql.cursors.DictCursor)
+    
+    def selectlist(self):
+        sql = "select * from emp"
+        self.cur.execute(sql)
+        
+        list = self.cur.fetchall()
+        return list
+    
+    def selectOne(self, e_id):
+        sql = f"""
+                select * from emp where e_id = {e_id}
+            """
+        self.cur.execute(sql)
+        
+        # vos = self.cur.fetchone()
+        # return vos[0]   ## 이 방법도 있음..
+        # fetchone : 배열의 [0]을 알아서 가져옴
+        vo = self.cur.fetchone()
+        return vo
+    
+    def insert(self, e_id, e_name, gen, addr):
+        sql = f"""
+                INSERT INTO emp VALUES ({e_id},{e_name},{gen},{addr})
+            """
+        cnt = self.cur.execute(sql)
+        self.conn.commit()
+        return cnt
+    
+    def delete(self, e_id):
+        sql = f"""
+                DELETE FROM emp 
+                WHERE e_id = '{e_id}'
+            """
+        cnt = self.cur.execute(sql)
+        self.conn.commit()
+        return cnt
+    
+    def update(self, e_id, e_name, gen, addr):
+        sql = f"""
+                UPDATE emp 
+                SET 
+                    e_name = '{e_name}',
+                    gen = '{gen}',
+                    addr = '{addr}'
+                WHERE e_id = '{e_id}'
+            """
+        cnt = self.cur.execute(sql)
+        self.conn.commit()
+        return cnt
+        
+    def __del__(self):
+        self.cur.close()
+        self.conn.close()
+        
+if __name__ == '__main__':
+    de = DaoEmp()
---
+++ HELLO_AJAX/HELLO_AJAX/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render
+from django.http.response import JsonResponse
+from flask import json
+from django.views.decorators.csrf import csrf_exempt
+from HELLO_AJAX.daoemp import DaoEmp
+
+
+@csrf_exempt
+def ajax(request):
+    dict = json.loads(request.body)
+    # do something
+    print(dict['e_id'])
+
+    context = {
+        'result': dict,
+    }
+    return JsonResponse(context)
+
+@csrf_exempt
+def ajax_selectlist(request):
+    
+    de = DaoEmp()
+    list = de.selectlist()
+
+    json = {
+        'list': list
+    }
+    return JsonResponse(json)
+
+@csrf_exempt
+def ajax_insert(request):
+    e_id = request.POST['e_id']
+    e_name = request.POST['e_name']
+    gen = request.POST['gen']
+    addr = request.POST['addr']
+    
+    de = DaoEmp()
+    cnt = de.insert(e_id, e_name, gen, addr)
+
+    json = {
+        'cnt': cnt
+    }
+    return JsonResponse(json)
+
+@csrf_exempt
+def ajax_update(request):
+    e_id = request.POST['e_id']
+    e_name = request.POST['e_name']
+    gen = request.POST['gen']
+    addr = request.POST['addr']
+    
+    de = DaoEmp()
+    cnt = de.update(e_id, e_name, gen, addr)
+
+    json = {
+        'cnt': cnt
+    }
+    return JsonResponse(json)
+
+@csrf_exempt
+def ajax_delete(request):
+    e_id = request.POST['e_id']
+    
+    de = DaoEmp()
+    cnt = de.delete(e_id)
+
+    json = {
+        'cnt': cnt
+    }
+    return JsonResponse(json)
+
+
+
